9b641f5fcc2f858ec7444bfd99352e140697c624
blakeBam <blakebambico@gmail.com>
2017-03-27 14:53:13 -0500

    Added some recursive functions for adding a node to the tree and finding a title in the tree

--- src/BSTree/Tree.java
+++ src/BSTree/Tree.java
@@ -1,3 +1,6 @@
+//bcb140130
+//CS 2336.002
+//Blake Bambico
 package BSTree;
 
 public class Tree {
@@ -7,9 +10,50 @@ public class Tree {
   
   public Tree(BaseNode r) {root = r;}
   
-  //todo define this method and some more
+  //helper method for the recursive adding function
   public void addNode(BaseNode a)
   {
-    
+    if(root == null) {root = a;}
+    else {add(a,root);}
+  }
+  
+  //recursive adding funtion, compare the string to each node and navigate the tree accordingly
+  private void add(BaseNode a, BaseNode start)
+  {
+    if(a.getTitle().compareTo(start.getTitle()) > 0)
+    {
+      if(start.getRight() != null) {add(a,start.getRight());}
+      else {start.setRight(a);}
+    }
+    else if(a.getTitle().compareTo(start.getTitle()) < 0)
+    {
+      if(start.getLeft() != null) {add(a,start.getLeft());}
+      else {start.setLeft(a);}
+    }
+    else {start.setAvailable(a.getAvailable());}
+  }
+  
+  //public helper method for finding a title in the tree
+  public BaseNode findTitle(String a)
+  {
+    if(root == null) {return null;}
+    else {return find(a,root);}
+  }
+  
+  //recursively search the tree for the title
+  private BaseNode find(String a, BaseNode c)
+  {
+    if(a.compareTo(c.getTitle()) > 0)
+    {
+      if(c.getRight() != null) {find(a,c.getRight());}
+      else {return null;}
+    }
+    else if(a.compareTo(c.getTitle()) < 0)
+    {
+      if(c.getLeft() != null) {find(a,c.getLeft());}
+      else {return null;}
+    }
+    else {return c;}
+    return null;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 //bcb140130
 //CS 2336.002
 //Blake Bambico
+import BSTree.*;
 
 public class Main {
   public static void main(String[] args) {
